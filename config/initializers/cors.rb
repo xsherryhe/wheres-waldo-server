1fc4cadb5bdbd9c421ea818ab763c7cd5d542bb9
Sherry He <xsherryhe@gmail.com>
2023-01-30 15:52:57 -0500

    Modified CORS origins to include https localhost

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3001', %r{\Ahttps?://xsherryhe.github.io}
+    origins %r{https?://localhost:3001}, %r{\Ahttps?://xsherryhe.github.io}
     resource '*', headers: :any, methods: %i[get post patch put options delete], credentials: true, expose: ['CSRF-TOKEN']
   end
 end
